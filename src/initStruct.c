d98527aaa0f84c5bf641be6fd72a6c3d4724ad3a
ceciliacal <c.calavaro@yahoo.it>
2021-10-08 13:43:41 +0200

    initlevel funzionante

--- src/initStruct.c
+++ src/initStruct.c
@@ -13,73 +13,61 @@ MODULE_LICENSE("GPL");
 
 tag_t *tagServiceArray[MAX_N_TAGS];
 
-/*
-void initTagService(){
+void initLevels(level_t* levelsArray[N_LEVELS]){
 
-    tag_t *tagServiceArray[256];
-
-
-}
- 
-int getNumOfTags(){
-    int len = sizeof tagServiceArray / sizeof tagServiceArray[0];
-    //printf("sizeof tagServiceArray = %d\n", sizeof tagServiceArray);
-    //printf("sizeof(tag_t) = %d\n", sizeof(tag_t));
-
-    return len;
-
-}
-*/
-
-int addTag(int key, uid_t userId){
-
-    tag_t* newTag;
-    
-
-    //prima devo inizializzare livelli
-    void *buf;
-
-    printf("sizeof(tag_t) = %d\n", sizeof(tag_t));
-    newTag = kzalloc(sizeof(tag_t), GFP_KERNEL);
-
-    return 0;
-
-
-
-
-}
-
-void initLevels(int num){
-
-    level_t* levelsArray[N_LEVELS];
     int i;
 
     for ( i=0;i<N_LEVELS;i++){
 
-        //level_t* newLev;
-        //newLev = kzalloc(sizeof(level_t),GFP_KERNEL);
 
-        levelsArray[i]= kzalloc(sizeof(level_t),GFP_KERNEL);
+        levelsArray[i]= (level_t*) kzalloc(sizeof(level_t),GFP_KERNEL);
         levelsArray[i]->num=i+1;
         printk("levelsArray[%d]= %d    num=%d\n",i,levelsArray[i],levelsArray[i]->num);
 
     }
 
+    printk("levelsArray= %d\n",levelsArray);    //ind array
+    printk("*levelsArray= %d\n",*levelsArray);  //primo elemen
+    printk("&levelsArray= %d\n",&levelsArray);  //ind array
+    printk("**levelsArray= %d\n",**levelsArray);  //ind array
+    //return *levelsArray;
 }
 
-/*
-Il check se chiave è ipc private, collegamento degli ID/key ecc va implementato 
-a livello kernel. 
 
-e pure per scrivere msg da user a kernel mesa che serve char device driver
+//int addTag(int key, uid_t userId){
+int addTag(int i){
 
+    tag_t* newTag;
+    newTag = (tag_t*) kzalloc(sizeof(tag_t), GFP_KERNEL);
 
-*/
-
-int init_module(){
+    level_t* levels[N_LEVELS];
     
-    initLevels(1);
+    initLevels(levels);
+    printk("dentro addTag (dopo initLevels): levels= %d\n",levels);
+    //printk("dentro addTag: newTag.levels= %d\n",newTag->levels);
+    
+    return 1;
+    
+}
 
+
+
+
+
+
+int init_module(void){
+    
+    level_t* levels[N_LEVELS];
+    addTag(1);
+    
     return 0;
 }
+
+
+
+void cleanup_module(void){
+    printk("CLEANUP!!\n");
+    
+}
+
 
